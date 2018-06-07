e237fafab14e39e285c02591354552810a2170bd
wade <wade@doubleservice.com>
2018-06-07 23:50:40 +0800

    feat: 實作新增 task 功能
    調整項目:
    1. css 加入 debug 用的顯示外框樣式
    2. 加入 jQuery template，供將來使用 template 渲染
    3. 實作 add-task 按鈕功能
    4. 新增 taskStorage.js 封裝 task Storage 之操作
    
    TODO:
    1. 新增 task 到 task Storage 之後，要連動把 task template 渲染在畫面上。

---
+++ week1-todolist/assets/tmpl/task.js
@@ -0,0 +1,15 @@
+<div class="task p-2 mt-2 rounded" draggable="true">
+    <div class="task-header pr-2">
+        <div class="d-inline-block">
+            <input type="checkbox" class="ml-2"/>
+            <span>${title}</span>
+        </div>
+        <div class="d-inline-block float-right">
+            <i class="far fa-lg fa-bookmark ml-2"></i> <i class="fas fa-lg fa-pencil-alt ml-2"></i>
+        </div>
+    </div>
+    <div class="task-footer">
+        <i class="far fa-calendar-alt ml-2"> ${deadline}</i> <i class="far fa-file ml-2"></i> <i
+            class="far fa-comment-dots ml-2"></i>
+    </div>
+</div>
