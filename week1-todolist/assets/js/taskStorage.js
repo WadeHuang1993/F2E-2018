e23cf969a9cdf0753ca9e5477a5df89d7e93a66e
wade <wade@doubleservice.com>
2018-06-08 09:26:27 +0800

    refactort: 加入 taskStorage 註解
    調整項目：
    1. 去除一層 taskStorage 函式，原函式單純 return 建構 taskStorage 函式，為了降低複雜度將此返回函數移除。
    2. 每個屬性加入可視度註解

--- week1-todolist/assets/js/taskStorage.js
+++ week1-todolist/assets/js/taskStorage.js
@@ -2,82 +2,108 @@
 
     "use strict";
 
+    /**
+     * 取得 sessionStorage 的 tasks 資料
+     *
+     * 每次載入 taskStorage.js 時，自動解析 sessionStorage.tasks
+     *
+     * @type {{}}
+     */
     var store = {};
     if ('undefined' != typeof(sessionStorage.tasks)) {
         store = JSON.parse(sessionStorage.tasks);
     }
 
+    /**
+     * 建構 taskStore 物件
+     *
+     * @param store
+     * @returns {taskStorage}
+     */
     var taskStorage = function (store) {
 
-        taskStorage = function (store) {
+        /**
+         * store 為 JSON.parse(sessionStorage.tasks) 解析後的物件。
+         *
+         * @access private
+         */
+        var store = store;
 
-            var store = store;
-
-            /**
-             * 取得 store 最新 id
-             *
-             * @returns {number} id
-             */
-            function getStoreId() {
-                var id = 0;
-                if ('undefined' != typeof (store.id)) {
-                    id = store.id;
-                }
-
-                return id;
+        /**
+         * 取得 store 最新 id
+         *
+         * @access private
+         * @returns {number} id
+         */
+        function getStoreId() {
+            var id = 0;
+            if ('undefined' != typeof (store.id)) {
+                id = store.id;
             }
 
-            function getNextStoreId() {
-                var id = getStoreId();
+            return id;
+        }
 
-                return ++id;
-            }
+        /**
+         * 取得下一個 Store id 流水號
+         *
+         * 供新增 task 用
+         *
+         * @access private
+         * @returns {number}
+         */
+        function getNextStoreId() {
+            var id = getStoreId();
 
-            /**
-             * 設置 tasks Storage
-             */
-            function setTaskStorage() {
+            return ++id;
+        }
 
-                sessionStorage.tasks = JSON.stringify(store);
-            }
+        /**
+         * 設置 tasks Storage
+         *
+         * @access private
+         */
+        function setTaskStorage() {
 
-            /**
-             * 增加 task store 之 id 流水號
-             *
-             * @param id number
-             */
-            function updateStoreId(id) {
-                store.id = id;
-            }
+            sessionStorage.tasks = JSON.stringify(store);
+        }
 
-            /**
-             * 新增一筆 task 至 task storage
-             *
-             * @param title
-             */
-            function create(title) {
+        /**
+         * 增加 task store 之 id 流水號
+         *
+         * @access private
+         * @param id number
+         */
+        function updateStoreId(id) {
+            store.id = id;
+        }
 
-                var id = getNextStoreId();
+        /**
+         * 新增一筆 task 至 task storage
+         *
+         * @access public
+         * @param title
+         */
+        function create(title) {
 
-                store[id] = {
-                    title: title,
-                    deadline: new Date()
-                };
+            var id = getNextStoreId();
 
-                updateStoreId(id);
+            store[id] = {
+                title: title,
+                deadline: new Date()
+            };
 
-                setTaskStorage();
-            }
+            updateStoreId(id);
 
+            setTaskStorage();
+        }
 
-            return {
-                create: create
-            }
-        };
-
-        return new taskStorage(store);
+        return {
+            create: create
+        }
     };
 
-    window.taskStorage = taskStorage(store);
+    // 在全局變數註冊 taskStorage 物件。
+    window.taskStorage = new taskStorage(store);
 
 })(document, window, 0);
